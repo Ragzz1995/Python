80999a02352432ecefd532279b7752a5b24b768c
Raghul Kannan <ragzz1995@gmail.com>
2016-12-19 14:55:44 +0530

    Update and rename pars_xml.py to geocodingl.py

--- geocodingl.py
+++ geocodingl.py
@@ -7,7 +7,7 @@ serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 address = raw_input('Enter location: ')
     
 
-url = serviceurl + urllib.urlencode({'address': address,'key':'AIzaSyDoszeuhFbv2LVtyxoYev0Pa3EPoUMmNpM'})
+url = serviceurl + urllib.urlencode({'address': address,'key':'Your google API key'})
 
 uh = urllib.urlopen(url)
 data = uh.read()
@@ -15,4 +15,3 @@ js = json.loads(str(data))
 print json.dumps(js["results"][0]["geometry"]["location"]["lat"],indent=4)
 print json.dumps(js["results"][0]["geometry"]["location"]["lng"],indent=4)
 
-'''AIzaSyDoszeuhFbv2LVtyxoYev0Pa3EPoUMmNpM'''
